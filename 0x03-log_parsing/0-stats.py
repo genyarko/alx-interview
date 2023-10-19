4d4e81fc625b45999e37c76b5d31415533b91c56
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-19 15:20:28 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,63 +1,67 @@
+#!/usr/bin/python3
+'''A script for parsing HTTP request logs.
+'''
+import sys
 import re
-
+import logging
 
 class InvalidInputError(Exception):
     pass
 
-
 def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
     '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is None:
-        raise InvalidInputError(f"Invalid input line: {input_line}")
-    status_code = resp_match.group('status_code')
-    file_size = int(resp_match.group('file_size'))
-    info['status_code'] = status_code
-    info['file_size'] = file_size
-    return info
+    Extracts sections of a line of an HTTP request log.
 
+    Args:
+        input_line (str): The input line to parse.
+
+    Returns:
+        dict: A dictionary containing the extracted information.
+    '''
+    log_format = re.compile(r'\s*(?P<ip>\S+)\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)]'
+                            r'\s*"(?P<request>[^"]*)"\s*(?P<status_code>\S+)'
+                            r'\s*(?P<file_size>\d+)')
+    
+    match = log_format.match(input_line)
+    if not match:
+        raise InvalidInputError(f"Invalid input line: {input_line}")
+    
+    return match.groupdict()
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
     '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+    Prints the accumulated statistics of the HTTP request log.
 
+    Args:
+        total_file_size (int): The total file size.
+        status_codes_stats (dict): A dictionary of status codes and their counts.
+    '''
+    logging.info(f'Total file size: {total_file_size}')
+    for code, count in sorted(status_codes_stats.items()):
+        if count > 0:
+            logging.info(f'{code}: {count}')
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
+    '''
+    Updates the metrics from a given HTTP request log.
 
     Args:
         line (str): The line of input from which to retrieve the metrics.
+        total_file_size (int): The current total file size.
+        status_codes_stats (dict): A dictionary of status codes and their counts.
 
     Returns:
         int: The new total file size.
     '''
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
+    if status_code in status_codes_stats:
         status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
+    return total_file_size + int(line_info['file_size'])
 
 def run():
-    '''Starts the log parser.
+    '''
+    Starts the log parser.
     '''
     line_num = 0
     total_file_size = 0
@@ -85,6 +89,6 @@ def run():
     except (KeyboardInterrupt, EOFError):
         print_statistics(total_file_size, status_codes_stats)
 
-
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     run()
