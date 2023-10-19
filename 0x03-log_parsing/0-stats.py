c23767714321684e6a819e85be398bc81c13a127
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-19 15:12:32 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,44 @@
 #!/usr/bin/python3
 import sys
 
-def is_valid_log_line(line):
-    # Check if the line matches the specified format
-    return all([
-        line.startswith('<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>'),
-        line.count('"GET /projects/260 HTTP/1.1"') == 1
-    ])
-
-def print_statistics(total_file_size, status_code_counts):
-    print(f'Total file size: {total_file_size}')
-    for code in sorted(status_code_counts):
-        count = status_code_counts[code]
-        if count > 0:
-            print(f'{code}: {count}')
-
-def main():
+def extract_metrics(line):
+    # Define a dictionary to hold the status code counts
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    
+    # Initialize variables for total file size and line count
     total_file_size = 0
-    status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
-
+    
     try:
         for line in sys.stdin:
-            line = line.strip()
-            if not is_valid_log_line(line):
-                continue
-
-            # Parse the line
-            _, _, _, _, status_code, file_size = line.split()
-            status_code = status_code.strip()
-            file_size = int(file_size)
-
-            # Update metrics
-            total_file_size += file_size
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-
             line_count += 1
+            elements = line.split()
+            
+            # Check if the line matches the expected format
+            if len(elements) >= 9:
+                status_code = elements[-2]
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                file_size = int(elements[-1])
+                total_file_size += file_size
 
-            # Print statistics after every 10 lines
+            # Print statistics every 10 lines or on keyboard interruption
             if line_count % 10 == 0:
-                print_statistics(total_file_size, status_code_counts)
+                print(f"Total file size: {total_file_size}")
+                for code in sorted(status_codes.keys()):
+                    count = status_codes[code]
+                    if count > 0:
+                        print(f"{code}: {count}")
 
     except KeyboardInterrupt:
-        pass
+        pass  # Handle keyboard interruption gracefully
 
-    print_statistics(total_file_size, status_code_counts)
+    finally:
+        print(f"Total file size: {total_file_size}")
+        for code in sorted(status_codes.keys()):
+            count = status_codes[code]
+            if count > 0:
+                print(f"{code}: {count}")
 
 if __name__ == '__main__':
-    main()
+    extract_metrics()
