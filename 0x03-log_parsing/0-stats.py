131959dc0427a9960cbe429ff9d2cbb49aee7dde
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-19 15:11:31 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,51 @@
 #!/usr/bin/python3
 import sys
 
-def parse_log_line(line):
-    """Parse a log line and return a tuple containing (status_code, file_size).
-    Return (None, None) if the line does not match the expected format.
-    """
-    elements = line.split()
-    if len(elements) >= 9:
-        status_code = elements[-2]
-        try:
-            status_code = int(status_code)
-            file_size = int(elements[-1])
-            return status_code, file_size
-        except ValueError:
-            return None, None
-    return None, None
+def is_valid_log_line(line):
+    # Check if the line matches the specified format
+    return all([
+        line.startswith('<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>'),
+        line.count('"GET /projects/260 HTTP/1.1"') == 1
+    ])
 
-def print_statistics(total_size, status_counts):
-    """Print the accumulated statistics."""
-    print(f"Total file size: {total_size}")
-    for code in sorted(status_counts.keys()):
-        count = status_counts.get(code, 0)
+def print_statistics(total_file_size, status_code_counts):
+    print(f'Total file size: {total_file_size}')
+    for code in sorted(status_code_counts):
+        count = status_code_counts[code]
         if count > 0:
-            print(f"{code}: {count}")
+            print(f'{code}: {count}')
 
 def main():
-    total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    total_file_size = 0
+    status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
 
     try:
         for line in sys.stdin:
-            status_code, file_size = parse_log_line(line)
-            
-            if status_code is not None and status_code in status_counts:
-                status_counts[status_code] += 1
-                total_size += file_size
-            
+            line = line.strip()
+            if not is_valid_log_line(line):
+                continue
+
+            # Parse the line
+            _, _, _, _, status_code, file_size = line.split()
+            status_code = status_code.strip()
+            file_size = int(file_size)
+
+            # Update metrics
+            total_file_size += file_size
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+
             line_count += 1
 
+            # Print statistics after every 10 lines
             if line_count % 10 == 0:
-                print_statistics(total_size, status_counts)
-    
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
+                print_statistics(total_file_size, status_code_counts)
 
-if __name__ == "__main__":
+    except KeyboardInterrupt:
+        pass
+
+    print_statistics(total_file_size, status_code_counts)
+
+if __name__ == '__main__':
     main()
