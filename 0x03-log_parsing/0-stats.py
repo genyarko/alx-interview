61bd1d878b6f58b33c605c50312a5d3d30a2b3a1
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-19 15:13:25 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
 import sys
 
-def extract_metrics(line):
-    # Define a dictionary to hold the status code counts
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-    
-    # Initialize variables for total file size and line count
-    total_file_size = 0
-    line_count = 0
-    
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            elements = line.split()
-            
-            # Check if the line matches the expected format
-            if len(elements) >= 9:
-                status_code = elements[-2]
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                file_size = int(elements[-1])
-                total_file_size += file_size
+# Initialize variables
+total_file_size = 0
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-            # Print statistics every 10 lines or on keyboard interruption
-            if line_count % 10 == 0:
-                print(f"Total file size: {total_file_size}")
-                for code in sorted(status_codes.keys()):
-                    count = status_codes[code]
-                    if count > 0:
-                        print(f"{code}: {count}")
+try:
+    line_number = 0
 
-    except KeyboardInterrupt:
-        pass  # Handle keyboard interruption gracefully
+    for line in sys.stdin:
+        line = line.strip()
 
-    finally:
-        print(f"Total file size: {total_file_size}")
-        for code in sorted(status_codes.keys()):
-            count = status_codes[code]
-            if count > 0:
-                print(f"{code}: {count}")
+        # Parse the line using a regular expression
+        import re
+        match = re.match(r'(\S+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line)
 
-if __name__ == '__main__':
-    extract_metrics()
+        if match:
+            status_code = int(match.group(3))
+            file_size = int(match.group(4))
+
+            # Update total file size
+            total_file_size += file_size
+
+            # Update status code count
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+
+        line_number += 1
+
+        if line_number % 10 == 0:
+            # Print statistics every 10 lines
+            print(f"Total file size: {total_file_size}")
+            for code in sorted(status_code_counts.keys()):
+                count = status_code_counts[code]
+                if count > 0:
+                    print(f"{code}: {count}")
+
+except KeyboardInterrupt:
+    pass  # Handle keyboard interruption gracefully
+
+finally:
+    # Print final statistics
+    print(f"Total file size: {total_file_size}")
+    for code in sorted(status_code_counts.keys()):
+        count = status_code_counts[code]
+        if count > 0:
+            print(f"{code}: {count}")
