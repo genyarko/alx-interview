9b8f8db3aaff533faa8fe993e4cfffabc8953157
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-02 10:58:52 +0000

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,62 +1,35 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, n):
-    # Check if it's safe to place a queen at board[row][col]
-    # Check the left side of the current row
-    for i in range(col):
-        if board[row][i] == 1:
+def is_valid(board, row, col, n):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
             return False
-
-    # Check the upper diagonal on the left
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check the lower diagonal on the left
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
-def solve_nqueens(n):
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
-
-    def solve(row):
-        if row == n:
-            # All queens are placed, add this solution to the list
-            solutions.append([[i, j] for i, j in enumerate(board)]
-        for col in range(n):
-            if is_safe(board, row, col, n):
-                board[row][col] = 1
-                solve(row + 1)
-                board[row][col] = 0
-
-    solve(0)
-    return solutions
-
-def print_solutions(solutions):
-    for solution in solutions:
-        for row in solution:
-            print(row)
-        print()
+def nqueens(board, row, n):
+    if row == n:
+        print(board)
+        return
+    for col in range(n):
+        if is_valid(board, row, col, n):
+            board[row] = col
+            nqueens(board, row + 1, n)
+            board[row] = -1
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    solutions = solve_nqueens(n)
-    print_solutions(solutions)
+    board = [-1] * n
+    nqueens(board, 0, n)
