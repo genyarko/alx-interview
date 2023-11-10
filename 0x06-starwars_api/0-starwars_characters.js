4d4e4926b3972399b7ffdbcf734e235a21fb5fb8
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-10 14:17:19 +0000

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,45 @@
 #!/usr/bin/node
+
 const request = require('request');
-const util = require('util');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-const promisifiedRequest = util.promisify(request);
+const API_URL = 'https://swapi.dev/api';
 
-async function getCharacters(movieId) {
-  try {
-    const filmResponse = await promisifiedRequest(`${API_URL}/films/${movieId}/`);
-    const charactersURL = JSON.parse(filmResponse.body).characters;
-
-    const charactersName = await Promise.all(
-      charactersURL.map(async (url) => {
-        const characterResponse = await promisifiedRequest(url);
-        return JSON.parse(characterResponse.body).name;
-      })
-    );
-
-    console.log(charactersName.join('\n'));
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-}
-
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  getCharacters(movieId);
-} else {
+if (process.argv.length !== 3) {
   console.error('Usage: ./script.js <Movie ID>');
   process.exit(1);
 }
 
+const movieId = process.argv[2];
+
+function getRequestPromise(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, _, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+
+function getMovieCharacters(movieId) {
+  const movieUrl = `${API_URL}/films/${movieId}/`;
+
+  getRequestPromise(movieUrl)
+    .then(movieData => {
+      const charactersURL = movieData.characters;
+      const charactersPromises = charactersURL.map(url => getRequestPromise(url));
+
+      return Promise.all(charactersPromises);
+    })
+    .then(charactersData => {
+      const characterNames = charactersData.map(data => data.name);
+      console.log(characterNames.join('\n'));
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+getMovieCharacters(movieId);
