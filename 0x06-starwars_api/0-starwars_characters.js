8b782d2e7d69321237a1dfeb9be9efc78c6136d6
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-10 14:20:28 +0000

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,22 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('request-promise-native');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-const API_URL = 'https://swapi.dev/api';
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./script.js <Movie ID>');
-  process.exit(1);
+async function getCharacterNames(filmId) {
+  try {
+    const filmData = await request(`${API_URL}/films/${filmId}/`);
+    const charactersURL = JSON.parse(filmData).characters;
+    const characterNamesPromises = charactersURL.map(async url => {
+      const characterData = await request(url);
+      return JSON.parse(characterData).name;
+    });
+    const characterNames = await Promise.all(characterNamesPromises);
+    console.log(characterNames.join('\n'));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-const movieId = process.argv[2];
-
-request(`${API_URL}/films/${movieId}/`, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.error(`Error: Unable to fetch data for Movie ID ${movieId}`);
-    process.exit(1);
-  }
-
-  const charactersURL = JSON.parse(body).characters;
-  const charactersPromises = charactersURL.map(url => {
-    return new Promise((resolve, reject) => {
-      request(url, (err, _, charactersReqBody) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(charactersReqBody).name);
-        }
-      });
-    });
-  });
-
-  Promise.all(charactersPromises)
-    .then(names => console.log(names.join('\n')))
-    .catch(err => console.error(err));
-});
-
+if (process.argv.length > 2) {
+  getCharacterNames(process.argv[2]);
+}
